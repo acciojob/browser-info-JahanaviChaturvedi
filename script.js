94ace4407d5672c790515ca7d23853f4eebd8087
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-26 18:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const browserName = navigator.appName; 
+const browserVersion = navigator.appVersion; 
+const userAgent = navigator.userAgent; 
+const browserInfoMessage = "You are using " + browserName + " version " + browserVersion;
+document.getElementById("browser-info").textContent = browserInfoMessage;
